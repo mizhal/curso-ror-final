67498be1c441c4467860e19eee5968034c55eda7
dicampus <dicampus@dicampus.dicampus>
2013-02-06 12:51:17 +0100

    Creación del repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyecto::Application.config.secret_token = '73bed04d3c7ab19d54350397b1f7715784b9264a7941d452cb8e76dfa808fe2a9624dc9a81dffefaeef2e55aa27c68caa42b4faba291803c18ade417914edd60'
