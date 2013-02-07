9f561f1bc4d9ad5912d84d5cfe05e3594c24e018
dicampus <dicampus@dicampus.dicampus>
2013-02-07 10:54:27 +0100

    Validación de Accommodation

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,7 +1,15 @@
 class Accommodation < ActiveRecord::Base
-  attr_accessible :address, :brief_description, :category_id, 
-    :city, :description, :featured, :landlord_id, :name, 
-    :postal_code, :province_id, :published
+  attr_accessible :address, 
+    :brief_description, 
+    :category_id, 
+    :city, 
+    :description, 
+    :featured, 
+    :landlord_id, 
+    :name, 
+    :postal_code, 
+    :province_id, 
+    :published
     
   belongs_to :province
   has_many :photos
@@ -14,4 +22,22 @@ class Accommodation < ActiveRecord::Base
   belongs_to :landlord, 
     :class_name => "User",
     :foreign_key => :landlord_id
+    
+  ### VALIDACIONES
+  #########################################################
+  
+  validates :address, :presence => true
+  validates :brief_description, :presence => true
+  validates :category_id, :presence => true
+  validates :city, :presence => true
+  validates :description, :presence => true
+  
+  validates :landlord_id, :presence => true
+  validates :name, :presence => true
+  validates :postal_code, :presence => true
+  validates :province_id, :presence => true
+  
+  ### FIN: VALIDACIONES
+  #########################################################
+
 end
