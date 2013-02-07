ca122f526a7843cbd70e4b8dc74b2f062392606c
dicampus <dicampus@dicampus.dicampus>
2013-02-07 13:00:06 +0100

    Refinamiento de restriccion de unidad

--- app/models/province.rb
+++ app/models/province.rb
@@ -6,8 +6,8 @@ class Province < ActiveRecord::Base
   
   ### VALIDACIONES
   #################################################
-  validates :name, :presence => true,
-    :uniqueness => true
+  validates :name, :presence => true
+  validates_uniqueness_of :name, :scope => :country_id
   validates :country_id, :presence => true
   
   ### fin: VALIDACIONES
