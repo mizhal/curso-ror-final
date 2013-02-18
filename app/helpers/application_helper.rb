530dafb713e127fe30d695bc803bcca39d210a90
dicampus <dicampus@dicampus.dicampus>
2013-02-19 00:15:12 +0100

    Mostrar booleanos en vistas de tabla como iconos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def true_false_icon guard
+    if guard 
+      return raw image_tag("true.png", 
+          :alt => t(".true", :default => "cierto")
+        ) 
+    else
+      return raw image_tag("false.png", 
+        :alt => t(".false", :default => "falso")
+      ) 
+    end
+  end
 end
