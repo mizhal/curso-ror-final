dc0d10b61afaaf4d0bb0c08d28ad46c18a12de9e
dicampus <dicampus@dicampus.dicampus>
2013-02-20 00:32:03 +0100

    Crear un helper para evitar la conducta indeseable del "content_for" que agrupa todo lo que se mande a una misma clave. De esta manera al usar el layout/partial de las cajas de datos cada caja nueva contenía su título y el de todas las anteriores.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
       ) 
     end
   end
+  
+  ####
+  ### funciona como content_for pero en lugar de agregar
+  ### el texto de todas las veces que se llama con la misma
+  ### clave como hace el content_for normal, elimina el texto
+  ### anterior que hubiera en dicha clave y escribe el nuevo
+  def set_content_for key
+    @view_flow.content.delete key
+    content_for key do
+      yield
+    end
+  end
 end
